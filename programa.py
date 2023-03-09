7f951dcda07a62de0433a11a0b86aeae606c6c79
Andrid1220 <126177267+Andrid1220@users.noreply.github.com>
2023-03-08 22:50:56 -0500

    programa para determinar la igualdad entre los dos ultimos digitos

---
+++ programa.py
@@ -0,0 +1,29 @@
+# programa para determinar la igualdad entre los dos ultimos digitos
+
+print("---------------------------------------")
+print("----IGUALDAD DE LOS ULTIMOS DIGITOS----")
+print("---------------------------------------")
+
+# input
+
+n = int(input("ingrese el entero: "))
+
+# procesing
+
+if (n>0):
+    ud = (n % 10)
+    p = (n % 100)//10
+    if (ud == p):
+       rta = "los dos ultimos digitos son iguales "
+    else:
+     rta = "los dos ultimos digitos no son iguales"
+else:
+    udN = -(n % 10)
+    pN = -(n % 100)//10
+    if (udN == pN ):
+       rta = "los dos ultimos digitos son iguales "
+    else:
+       rta = "los dos ultimos digitos no son iguales"
+
+# output
+print(str(rta))
